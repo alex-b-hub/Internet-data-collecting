69457ba9d1a441c35c126c85ad162142cdc57822
Aleksei Bakeev <87610585+s2eMailRu@users.noreply.github.com>
2022-05-03 14:15:08 +0300

    Add files via upload

---
+++ jobparser/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JobparserItem(scrapy.Item):
+    name = scrapy.Field()
+    company = scrapy.Field()
+    salary = scrapy.Field()
+    min_salary = scrapy.Field()
+    max_salary = scrapy.Field()
+    currency = scrapy.Field()
+    url = scrapy.Field()
+    place = scrapy.Field()
+    _id = scrapy.Field()
+
